4b975ed5ee559c9fd03984d1ace4d65a23f0cf06
sologuboved <sologuboved@gmail.com>
2017-07-18 15:46:50 +0300

    scrape Old Testament

--- scrape_figures.py
+++ scrape_figures.py
@@ -1,8 +1,13 @@
+# -*- coding: utf-8 -*-
+
 import requests
 from bs4 import BeautifulSoup
 import json
 
-FIGURES_OLD = 'figures_old.json'
+PREFIX = 'https://ru.wikisource.org'
+URL_OLD = 'https://ru.wikisource.org/wiki/Ветхий_завет'
+FIGURES_OLD_CANON = 'figures_old_canon.json'
+BOOK_NAMES_OLD_CANON = 'book_names_old_canon.json'
 
 
 def load_json(json_file):
@@ -11,28 +16,53 @@ def load_json(json_file):
 
 
 def dump_json(data, json_file):
+    print "Dumping", json_file
     with open(json_file, 'w') as handler:
         json.dump(data, handler)
 
 
-def scrape_book(url):
+def scrape_old_canon(prefix, url):
+    figures = dict()
+    book_names = dict()
+
     soup = BeautifulSoup(requests.get(url).content)
-    verses = [map(int, item.get('id').split(':')) for item in soup.find_all('span', {'style': 'color:#00F;'})]
-    print verses[-1]
+
+    table = soup.find('table', {'class': 'standard'})
+
+    for row in table.find_all('tr')[1:]:
+        cells = row.find_all('td')
+
+        en_book_name = cells[5].text
+        ru_book_name = cells[0].find_all('a')[0].text
+        book_url = prefix + cells[0].find_all('a')[0].get('href')
+
+        book_names[en_book_name] = ru_book_name
+        figures[en_book_name] = scrape_book(en_book_name, book_url)
+
+    return book_names, figures
+
+
+def scrape_book(en_book_name, url):
+    print 'Scraping', en_book_name
     verses_per_chapter = list()
+
+    soup = BeautifulSoup(requests.get(url).content)
+
+    verses = [map(int, item.get('id').split(':')) for item in soup.find_all('span', {'style': 'color:#00F;'})]
+
     previous_chapter_num = 1
     last_verse_num = None
     for curr_chapter_num, curr_verse_num in verses:
         if curr_chapter_num != previous_chapter_num:
-            print curr_verse_num, last_verse_num
             verses_per_chapter.append(last_verse_num)
             previous_chapter_num = curr_chapter_num
             last_verse_num = curr_verse_num
         else:
             last_verse_num = curr_verse_num
+
     verses_per_chapter.append(verses[-1][1])
-    print verses_per_chapter, len(verses_per_chapter)
+    return verses_per_chapter
 
 
 if __name__ == '__main__':
-    scrape_book('https://ru.wikisource.org/wiki/%D0%9F%D1%81%D0%B0%D0%BB%D1%82%D0%B8%D1%80%D1%8C')
+    pass
