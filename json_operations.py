6bb91d35ebc8965c3d85fc0289fb71bf34b371aa
sologuboved <sologuboved@gmail.com>
2017-07-19 14:21:15 +0300

    added json_operations

---
+++ json_operations.py
@@ -0,0 +1,12 @@
+import json
+
+
+def load_json(json_file):
+    with open(json_file) as data:
+        return json.load(data)
+
+
+def dump_json(data, json_file):
+    print "Dumping", json_file
+    with open(json_file, 'w') as handler:
+        json.dump(data, handler)
