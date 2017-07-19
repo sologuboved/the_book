0dc4a74cf0bd005633bd717334612e347b28caa3
sologuboved <sologuboved@gmail.com>
2017-07-19 11:59:44 +0300

    added files with data

---
+++ stats.py
@@ -0,0 +1,8 @@
+import json
+
+FIGURES_OLD_CANON = 'figures_old_canon.json'
+BOOK_NAMES_OLD_CANON = 'book_names_old_canon.json'
+
+class Figures(object):
+    def __init__(self, names_file, figures_file):
+
